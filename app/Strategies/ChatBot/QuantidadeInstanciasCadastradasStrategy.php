78f892e365ed7f5d47008a4434cdcf3766dc2fbb
Yuri Ferreira <yuri.ferreira@flexpeak.com.br>
2022-07-19 16:24:21 -0400

    try

--- app/Strategies/ChatBot/QuantidadeInstanciasCadastradasStrategy.php
+++ app/Strategies/ChatBot/QuantidadeInstanciasCadastradasStrategy.php
@@ -11,6 +11,7 @@ use App\Models\Local;
 use App\Models\Sinodal;
 use App\Models\User;
 use App\Services\IClaudiaService;
+use Illuminate\Support\Facades\Log;
 
 class QuantidadeInstanciasCadastradasStrategy implements ChatBotStrategy
 {
@@ -29,26 +30,29 @@ class QuantidadeInstanciasCadastradasStrategy implements ChatBotStrategy
 
     public static function getTotalizador(BotCliente $cliente)
     {
-        
-        $usuario = $cliente->usuario;
-        $texto = '';
-        if ($usuario->hasRole('diretoria')) {
-            $texto .= self::getTotalizadorSinodais($usuario) . PHP_EOL;
-            $sinodais = Sinodal::whereIn('regiao_id', $usuario->regioes->pluck('id'))->get()->pluck('id');
-            $texto .= self::getTotalizadorFederacoes($sinodais) . PHP_EOL;
-            $federacoes = Federacao::whereIn('sinodal_id', $sinodais)->get()->pluck('id');
-            $texto .= self::getTotalizadorLocais($federacoes) . PHP_EOL;
-        }
-        if ($usuario->hasRole('sinodal')) {
+       try {
+            $usuario = $cliente->usuario;
+            $texto = '';
+            if ($usuario->hasRole('diretoria')) {
+                $texto .= self::getTotalizadorSinodais($usuario) . PHP_EOL;
+                $sinodais = Sinodal::whereIn('regiao_id', $usuario->regioes->pluck('id'))->get()->pluck('id');
+                $texto .= self::getTotalizadorFederacoes($sinodais) . PHP_EOL;
+                $federacoes = Federacao::whereIn('sinodal_id', $sinodais)->get()->pluck('id');
+                $texto .= self::getTotalizadorLocais($federacoes) . PHP_EOL;
+            }
+            if ($usuario->hasRole('sinodal')) {
 
-            $texto .= self::getTotalizadorFederacoes($usuario->sinodais->pluck('id')) . PHP_EOL;
-            $federacoes = Federacao::whereIn('sinodal_id', $usuario->sinodais->pluck('id'))->get()->pluck('id');
-            $texto .= self::getTotalizadorLocais($federacoes) . PHP_EOL;
-        }
-        if ($usuario->hasRole('federacao')) {
-            $texto .= self::getTotalizadorLocais($usuario->federacoes->pluck('id')) . PHP_EOL;
-        }
-        return $texto;
+                $texto .= self::getTotalizadorFederacoes($usuario->sinodais->pluck('id')) . PHP_EOL;
+                $federacoes = Federacao::whereIn('sinodal_id', $usuario->sinodais->pluck('id'))->get()->pluck('id');
+                $texto .= self::getTotalizadorLocais($federacoes) . PHP_EOL;
+            }
+            if ($usuario->hasRole('federacao')) {
+                $texto .= self::getTotalizadorLocais($usuario->federacoes->pluck('id')) . PHP_EOL;
+            }
+            return $texto;
+       } catch (\Throwable $th) {
+           Log::error([$th->getMessage(), $th->getLine()]);
+       }
     }
 
     public static function getTotalizadorSinodais(User $user)
