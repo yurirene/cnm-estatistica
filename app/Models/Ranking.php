04bc4571ed5ab1500ebe8e59c80591790b6b44c3
Yuri Ferreira <yurirene@gmail.com>
2023-01-27 01:21:01 -0400

    pegando posição

--- app/Models/Ranking.php
+++ app/Models/Ranking.php
@@ -10,4 +10,11 @@ class Ranking extends Model
     protected $table = 'rankings';
     protected $guarded = ['id', 'created_at', 'updated_at'];
     
+    public function getExplicacaoDetalhadaAttribute()
+    {
+        $texto_federacao = $this->form_fed_entregue > 1 ? 'formulários de Federações' : 'formulário de Federação'; 
+        $texto_local = $this->form_local_entregue > 1 ? 'formulários de UMPs Locais' : 'formulário de UMP Local';
+        return "Sua posição é resultado da entrega de {$this->form_fed_entregue} {$texto_federacao} 
+        e {$this->form_local_entregue} {$texto_local}. Total de Pontos: {$this->pontuacao} de 100";
+    }
 }
