62ed8e0bfa34216152d92efeab585a7e90417ee8
Yuri Ferreira <yurirene@gmail.com>
2023-04-03 04:10:20 +0000

    ajustes

--- resources/views/errors/500.blade.php
+++ resources/views/errors/500.blade.php
@@ -2,5 +2,5 @@
 <img src="/img/erros/500.svg" style="width:40%">
 </div>
 <h2 style="color: #4b008f; text-align:center">
-Algo deu errado. Mas já fomos notificados e trabalheremos para corrigir...
+Algo deu errado. Mas já fomos notificados e trabalharemos para corrigir...
 </h2>
