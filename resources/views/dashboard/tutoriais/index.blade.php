d5005aa964e1b750a892900519cd61db0848a704
Yuri Ferreira <yuri.ferreira@superlogica.com>
2024-01-13 19:55:51 -0300

    adicionando tutorial usuarios

--- resources/views/dashboard/tutoriais/index.blade.php
+++ resources/views/dashboard/tutoriais/index.blade.php
@@ -5,7 +5,7 @@
 @include('dashboard.partes.head', [
     'titulo' => 'Tutoriais'
 ])
-    
+
 <div class="container-fluid mt--7">
     <div class="row mt-5">
         <div class="col-xl-12 mb-5 mb-xl-0">
@@ -25,14 +25,14 @@
                                     Formulário Estatístico - UMP Local
                                 </div>
                                 <div class="card-body">
-                                    
-                                    <iframe 
-                                        width="100%" 
-                                        height="315" 
-                                        src="https://www.youtube.com/embed/UrbtSnbrk9k" 
-                                        title="YouTube video player" 
-                                        frameborder="0" 
-                                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+
+                                    <iframe
+                                        width="100%"
+                                        height="315"
+                                        src="https://www.youtube.com/embed/UrbtSnbrk9k"
+                                        title="YouTube video player"
+                                        frameborder="0"
+                                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
                                         allowfullscreen
                                     ></iframe>
                                 </div>
@@ -44,15 +44,15 @@
                                     Formulário Estatístico - Federação
                                 </div>
                                 <div class="card-body">
-                                    
-                                    
-                                    <iframe 
-                                        width="100%" 
-                                        height="315" 
-                                        src="https://www.youtube.com/embed/6eBoaLOhLFQ" 
-                                        title="YouTube video player" 
-                                        frameborder="0" 
-                                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+
+
+                                    <iframe
+                                        width="100%"
+                                        height="315"
+                                        src="https://www.youtube.com/embed/6eBoaLOhLFQ"
+                                        title="YouTube video player"
+                                        frameborder="0"
+                                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
                                         allowfullscreen
                                     ></iframe>
                                 </div>
@@ -64,15 +64,29 @@
                                     Formulário Estatístico - Sinodal
                                 </div>
                                 <div class="card-body">
-                                    <iframe 
-                                        width="100%" 
-                                        height="315" 
-                                        src="https://www.youtube.com/embed/LrhLb5opZXA" 
-                                        title="YouTube video player" 
-                                        frameborder="0" 
-                                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+                                    <iframe
+                                        width="100%"
+                                        height="315"
+                                        src="https://www.youtube.com/embed/LrhLb5opZXA"
+                                        title="YouTube video player"
+                                        frameborder="0"
+                                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
                                         allowfullscreen
                                     ></iframe>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-4 col-sm-12 p-2">
+                            <div class="card shadow">
+                                <div class="card-header">
+                                    Cadastro de Usuários
+                                </div>
+                                <div class="card-body">
+                                    <a href="/downloads/tutorial_usuario.pdf" class="btn btn-primary"
+                                        target="_blank"
+                                    >
+                                        <i class="fas fa-file"></i> Visualizar
+                                    </a>
                                 </div>
                             </div>
                         </div>
@@ -80,7 +94,7 @@
                 </div>
             </div>
         </div>
-        
+
     </div>
 </div>
 @endsection
