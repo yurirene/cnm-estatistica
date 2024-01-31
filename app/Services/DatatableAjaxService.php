6315d3459517e000e3e93ab47f5facab7817ea8d
Yuri Ferreira <yuri.ferreira@superlogica.com>
2024-01-31 03:13:53 -0300

    filtrando apenas pelos entregues de verdade

--- app/Services/DatatableAjaxService.php
+++ app/Services/DatatableAjaxService.php
@@ -208,12 +208,15 @@ class DatatableAjaxService
             $formulariosEntregues = $query
                 ->where('status', true)
                 ->get()
-                ->map(function ($item) {
+                ->map(function ($item) use ($instancia){
                     return [
                         'id' => $item->id,
                         'nome' => $item->nome,
                         'entregue' => $item->relatorios()
                             ->where('ano_referencia', EstatisticaService::getAnoReferencia())
+                            ->when($instancia != 'Local', function ($sql) {
+                                return $sql->where('status', EstatisticaService::FORMULARIO_ENTREGUE);
+                            })
                             ->get()
                             ->count(),
                     ];
